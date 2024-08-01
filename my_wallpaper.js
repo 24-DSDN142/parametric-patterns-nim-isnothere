352f3786c1a4e51f305406f81cf73f8f76f5f113
nim-isnothere <leesmnina@myvuw.ac.nz>
2024-08-02 11:00:48 +1200

    Actual Final Version

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,12 +1,12 @@
 //parameter variables and custom functions are here!
-let FishSize = 1
-let WaveStroke = [194,83,83]
-let WaveLight = [255,114,114]
-let WaveDark = [249,154,154]
-let LilypadStroke = [154,103,42]
-let LilypadFill = [234,190,134]
-let FishyFill = [255,244,230]
-let FishyStroke = [255,165,123]
+let FishSize = 0.5
+let WaveStroke = [255,165,123]
+let WaveLight = [255,244,230]
+let WaveDark = [255]
+let LilypadStroke = [255,114,114]
+let LilypadFill = [255,207,204]
+let FishyFill = [120,64,44]
+let FishyStroke = [86,47,33]
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
@@ -19,10 +19,11 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.grid_settings.row_offset  = 0;
   angleMode(DEGREES)
 }
+
 function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
 }
-function ScaleRow1(){
+function ScaleRow1(){ //row of 2 waves in the background
   stroke(WaveStroke)
   fill(WaveLight)
   arc(50,y,100,100,180,0);
@@ -35,7 +36,7 @@ function ScaleRow1(){
   arc(150,y,60,60,180,0);
   arc(150,y,40,40,180,0);
   arc(150,y,20,20,180,0)}
-function ScaleRow2(){
+function ScaleRow2(){ //row of 3 waves in the background
 stroke(WaveStroke)
 fill(WaveDark)
 arc(200,y,100,100,180,270);
@@ -49,12 +50,12 @@ arc(100,y,60,60,180,0);
 arc(100,y,40,40,180,0);
 arc(100,y,20,20,180,0)
 arc(0,y,100,100,270,0);
-arc(0,y,80,80,270,0);
-arc(0,y,60,60,270,0);
-arc(0,y,40,40,270,0);
-arc(0,y,20,20,270,0)
+arc(0,y,80,80,269,0);
+arc(0,y,60,60,269,0);
+arc(0,y,40,40,269,0);
+arc(0,y,20,20,269,0)
 }
-function Lilypad(){
+function Lilypad(){ //lilypad shape and position
 stroke(LilypadStroke)
 fill(LilypadFill)
 arc (170,150,50,50,270,210)
@@ -63,13 +64,13 @@ line (170,150,170,125)
 line (30,50,30,25)
 line (170,150,148.5,137.5)
 line (30,50,51.5,37.5)}
-function Fishy(){ 
+function Fishy(){ //fish shape and position
 stroke(FishyStroke)
 fill(FishyFill)
 arc(130,50,70,50,330,240)
 line(130,50,157,34)
 line(130,50,117,27)
-noStroke()
+stroke(FishyFill)
 triangle(92,157,52,100,130,60)
 triangle(92,157,52,100,70,130)
 triangle(130,50,130,70,100,77)
@@ -85,9 +86,6 @@ noFill()
 arc(95,135,15,15,240,30)
 arc(100,130,18,18,240,30)}
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
- //arc(midx,midy,w,h,start,end)
- //ellipse (midx,midy,w,h,vertices)
- //curve (angx1, angy1, rightx, righty, leftx, lefty, angx2, angy2) 
  if (FishSize == 0.5){
     ScaleRow1 (y = 25)
     ScaleRow2 (y = 50)
@@ -103,8 +101,9 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
     Fishy()
     translate(180,-150)
     Fishy()}
-  if (FishSize == 1){
-    ScaleRow1 (y = 25)
+
+  else {
+   ScaleRow1 (y = 25)
     ScaleRow2 (y = 50)
     ScaleRow1 (y=75)
     ScaleRow2 (y = 100)
